58e8f5a1287c24f6e42bcf704be169a405341e4d
mihaf <fahimeh2067@gmail.com>
2024-01-02 18:25:14 +0330

    mihaf joon

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+
+
+
+const tick=()=>{
+
+const elem = (
+  <div>
+    <h1>سلام دوستان من</h1>
+    <h2>
+      it is {new Date().toLocaleTimeString()}
+    </h2>
+  </div>
+  
+  )
+ReactDOM.render(elem,document.getElementById('root'));
+}
+
+setInterval(() => {
+  tick();
+  }, 1000);
+
+
+
+ 
+ 
+ 
